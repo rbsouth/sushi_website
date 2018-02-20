5e503676bd1f1ec1790052b9f5b1144b56f88abf
Amber Blackwell <ambereblackwell@gmail.com>
2018-02-20 10:25:19 -0700

    modal created in layout vew and main rb. need to pull user profile functionality for further testing.

--- main.rb
+++ main.rb
@@ -6,6 +6,8 @@ require 'date'
 enable :sessions
 set :database, 'sqlite3:blog.sqlite3'
 
+current_user = User.find(session[:user])
+
 get '/' do
   erb :sign_in, :layout => :layout_login
 end
@@ -43,3 +45,8 @@ get '/profile/:id' do
 	erb :profile
 end
 
+post '/new-post' do
+	current_user.posts.create(created_at: Time.now, content: params[:post][:content], picture: params[:post][:picture])
+	#if picture field is empty, add .hide to picture div class 
+	redirect :home
+end
